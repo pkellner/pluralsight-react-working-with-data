1187313bddd71bbd16095eeb3d8dfd2ce783f5be
pkellner <peter@peterkellner.net>
2023-12-14 07:34:04 -0800

    wip

--- m0/clip-000-convert-to-suspense/src/app/footer-subscribe-action.ts
+++ m0/clip-000-convert-to-suspense/src/app/footer-subscribe-action.ts
@@ -13,6 +13,31 @@ export default async function addAttendeeAction(
   formData: FormData,
 ) {
   const email = formData.get("email") as string;
+  const id = formData.get("id") as string;
+  const firstName = formData.get("firstName") as string;
+  const lastName = formData.get("lastName") as string;
+
+  await sleep(2000);
+
+  if (id && id.length > 0) {
+    await prisma.attendee.update({
+      where: {
+        id: id,
+      },
+      data: {
+        firstName: firstName,
+        lastName: lastName,
+      },
+    });
+
+    revalidatePath("/");
+
+    return {
+      message: "Updated attendee " + email,
+      step: "DONE",
+    };
+
+  }
 
   const attendee = {
     id: createGUID(),
@@ -23,8 +48,6 @@ export default async function addAttendeeAction(
   };
 
   try {
-    await sleep(3000);
-
     const count = await prisma.attendee.count({
       where: {
         email: email,
@@ -37,14 +60,14 @@ export default async function addAttendeeAction(
     const newAttendee = await prisma.attendee.create({
       data: attendee,
     });
-    console.log(
-      "/app/actions.ts: addAttendeeAction: newAttendee:",
-      newAttendee,
-    );
     revalidatePath("/");
-    return { message: "Added attendee " + email };
+    // when we pass back an id, it will be used to update the attendee in step2.
+    return {
+      message: `The email ${email ?? "unknown"} has been subscribed for updates.`,
+      step: "STEP2",
+      id: newAttendee.id,
+    };
   } catch (e) {
-    console.log("/app/actions.ts: addAttendeeAction: e:", e);
     return { message: "error: Failed to create attendee from action" };
   }
 }
---
+++ m0/clip-000-convert-to-suspense/src/app/footer-subscribe-just-email.tsx
@@ -0,0 +1,54 @@
+"use client";
+import React from "react";
+import addAttendeeAction from "@/app/footer-subscribe-action";
+import { useFormState, useFormStatus } from "react-dom";
+
+// for pending to work, this must be a child component of a form, not in the form itself
+function SubmitButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      type="submit"
+      className="btn btn-outline-dark speaker-rounded-corners"
+    >
+      {pending ? "Subscribing..." : "Subscribe"}
+    </button>
+  );
+}
+
+export default function FooterSubscribeJustEmail() {
+  const initialState = {
+    message: "",
+  };
+
+  const [state, formAction] = useFormState(addAttendeeAction, initialState);
+
+  return (
+    <div>
+      <h5 className="text-uppercase mb-4">Stay Updated</h5>
+      <form action={formAction}>
+        <div className="d-flex align-items-center">
+          <input
+            type="email"
+            className="form-control me-2 speaker-rounded-corners"
+            placeholder="Email address"
+            required
+            id="email"
+            name="email"
+          />
+          <SubmitButton />
+        </div>
+        {state?.message && (
+          <div
+            className={`mt-3 text-${
+              state.message.startsWith("error:") ? "danger" : "primary"
+            }`}
+          >
+            {state.message}
+          </div>
+        )}
+      </form>
+    </div>
+  );
+}
