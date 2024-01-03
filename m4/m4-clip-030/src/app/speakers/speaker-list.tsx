83ce2298eb79650d609c6da91e88115afaa3b27c
pkellner <peter@peterkellner.net>
2024-01-03 15:28:30 -0800

    go

---
+++ m4/m4-clip-030/src/app/speakers/speaker-list.tsx
@@ -0,0 +1,29 @@
+import SpeakerDetail from "@/app/speakers/speaker-detail";
+import { Speaker } from "@/lib/general-types";
+import { useSpeakerDataContext } from "@/contexts/speaker-data-context";
+
+
+
+export default function SpeakerList() {
+  
+  const { speakerState } = useSpeakerDataContext();
+
+  if (speakerState.loadingStatus === "error") {
+    return <div className="card">Error: {speakerState.error}</div>;
+  }
+
+  if (speakerState.loadingStatus === "loading") {
+    return <div>Loading ...</div>;
+  }
+
+  return (
+    <div className="container">
+      <div className="row g-4">
+        {speakerState.speakerList.map(function (speaker: Speaker) {
+          return <SpeakerDetail key={speaker.id} speaker={speaker} />;
+        })}
+      </div>
+    </div>
+  );
+
+}
