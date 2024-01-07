3df8e1256ef523f2b7af470f291e6c9162a93819
pkellner <peter@peterkellner.net>
2024-01-07 13:59:20 -0800

    go

---
+++ m5/m5-clip-020/experimental/src/app/error-boundary.jsx
@@ -0,0 +1,26 @@
+import { Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error("Error:", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
--- m5/m5-clip-020/experimental/src/app/speakers-with-suspense/speaker-favorite-count.tsx
+++ m5/m5-clip-020/experimental/src/app/speakers-with-suspense/speaker-favorite-count.tsx
@@ -1,21 +1,75 @@
+import { useState, useMemo } from "react";
 import SpeakerFavoriteCountDisplay from "@/app/speakers-with-suspense/speaker-favorite-count-display";
-import {Suspense} from "react";
+import { Suspense } from "react";
+import ErrorBoundary from "@/app/error-boundary";
 
-export default function SpeakerFavoriteCount({ speakerId }: { speakerId: number}) {
+function SuspenseFallback() {
+  return (
+    <div>
 
-  const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+      <button disabled
+              className="btn btn-primary m-3"
+      >
+        Refresh
+      </button>
+       <span className="text-muted">Favorite Count: *</span>
+    </div>
+  )
+}
+
+export default function SpeakerFavoriteCount({
+                                               speakerId,
+                                             }: {
+  speakerId: number;
+}) {
+  const sleep = (ms: number) =>
+    new Promise((resolve) => setTimeout(resolve, ms));
 
   async function fetchFavoriteCount() {
-    const randomSleep = Math.floor(1000 + Math.random() * 5000) + 1;
+    const randomSleep = Math.floor(1000 + Math.random() * 2000) + 1;
     await sleep(randomSleep);
-    const response = await fetch(`http://localhost:3000/api/speakers/${speakerId}`);
+    if (randomSleep > 100000)
+      throw new Error("Took too long to fetch favorite count");
+    const response = await fetch(
+      `http://localhost:3000/api/speakers/${speakerId}`,
+    );
     const data = await response.json();
     return data.favoriteCount;
   }
 
-  const favoriteCountPromise = fetchFavoriteCount();
+  const [dependency, setDependency] = useState(0);
 
-  return <Suspense fallback={<div className="text-muted">Favorite Count: *</div>}>
-    <SpeakerFavoriteCountDisplay favoriteCountPromise={favoriteCountPromise} />
-  </Suspense>
+  // const favoriteCountPromise = fetchFavoriteCount();
+  const favoriteCountPromise = useMemo(
+    () => fetchFavoriteCount(),
+    [dependency],
+  );
+
+  // <Suspense fallback={<div className="text-muted">Favorite Count: *</div>}>
+
+
+  return (
+    <ErrorBoundary
+      fallback={
+        <div className="text-danger">
+          Error Occurred when loading Favorite Count..
+        </div>
+      }
+    >
+      <Suspense fallback={<SuspenseFallback />}>
+        <div>
+          <button
+            className="btn btn-primary m-3"
+            onClick={() => setDependency(dependency + 1)}
+          >
+            Refresh
+          </button>
+          <SpeakerFavoriteCountDisplay
+            favoriteCountPromise={favoriteCountPromise}
+          />
+
+        </div>
+      </Suspense>
+    </ErrorBoundary>
+  );
 }
