89cf6af7a503d2d8a3b7c8b428104a8642a30ab6
pkellner <peter@peterkellner.net>
2024-01-12 12:26:34 -0800

    go

--- m6/m6-clip-030-add-favor-final/src/app/speakers/speaker-list.tsx
+++ m6/m6-clip-030-add-favor-final/src/app/speakers/speaker-list.tsx
@@ -13,10 +13,18 @@ export default function SpeakerList() {
     return <div>Loading ...</div>;
   }
 
+  const speakerListSorted = speakerState.speakerList.sort(
+    (a: Speaker, b: Speaker) => {
+      const nameA = a.lastName + a.firstName;
+      const nameB = b.lastName + b.firstName;
+      return nameA.localeCompare(nameB);
+    },
+  );
+
   return (
     <div className="container pb-4">
       <div className="row g-4">
-        {speakerState.speakerList.map(function (speaker: Speaker) {
+        {speakerListSorted.map(function (speaker: Speaker) {
           return <SpeakerDetail key={speaker.id} speaker={speaker} />;
         })}
       </div>
