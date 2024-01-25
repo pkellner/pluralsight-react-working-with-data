23011edba06436121018ac5ebad83261eb01d769
pkellner <peter@peterkellner.net>
2024-01-25 08:11:42 -0800

    o

---
+++ m7/m7-clip-070/src/app/server-action-example/page-server-action.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import prisma from "@/lib/prisma/prisma";
+import { redirect } from "next/navigation";
+import { randomUUID } from "node:crypto";
+
+export async function addAttendeeAction(formData: FormData) {
+
+  await new Promise<void>((resolve) => setTimeout(resolve, 3000));
+  
+  const attendeeRec = {
+    id: randomUUID() as string,
+    email: formData.get("email") as string,
+    firstName: formData.get("firstName") as string,
+    lastName: formData.get("lastName") as string,
+    createdDate: new Date(),
+  }
+  await prisma.attendee.create({ data: attendeeRec, });
+  redirect("/server-component-example");
+}
---
+++ m7/m7-clip-070/src/app/server-component-example/page.tsx
@@ -0,0 +1,36 @@
+import { Attendee } from ".prisma/client";
+import prisma from "@/lib/prisma/prisma";
+import Link from "next/link";
+
+export default async function ListAttendees() {
+  const attendees = await prisma.attendee.findMany({
+    orderBy: { createdDate: "desc", },
+    take: 10,
+  });
+
+  return (
+    <div className="table-responsive">
+      <Link href={"/server-action-example"}>Back to form</Link>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Created Date - id</th>
+          </tr>
+        </thead>
+        <tbody>
+          {attendees.map((attendee: Attendee) => (
+            <tr key={attendee.id}>
+              <td>{attendee.firstName}</td>
+              <td>{attendee.lastName}</td>
+              <td>{attendee.email}</td>
+              <td>{attendee.createdDate.toLocaleTimeString()} - {attendee.id}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
